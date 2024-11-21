01311206c7c22e93d7661eef33d1164fa44fd148
Matt Fishman <mtfishman@users.noreply.github.com>
2024-11-21 14:23:29 -0500

    Add `generate` function (#2)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ITensorPkgSkeleton
+# ITensorPkgSkeleton.jl
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://ITensor.github.io/ITensorPkgSkeleton.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://ITensor.github.io/ITensorPkgSkeleton.jl/dev/)
@@ -6,3 +6,11 @@
 [![Coverage](https://codecov.io/gh/ITensor/ITensorPkgSkeleton.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/ITensor/ITensorPkgSkeleton.jl)
 [![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/invenia/BlueStyle)
 [![Aqua](https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg)](https://github.com/JuliaTesting/Aqua.jl)
+
+```julia
+using ITensorPkgSkeleton: ITensorPkgSkeleton
+# This step might be required to circumvent issues with
+# the version of git installed by `Git.jl`.
+ITensorPkgSkeleton.use_system_git!()
+ITensorPkgSkeleton.generate("NewPkg")
+```
--- src/ITensorPkgSkeleton.jl
+++ src/ITensorPkgSkeleton.jl
@@ -1,5 +1,75 @@
 module ITensorPkgSkeleton
 
-# Write your package code here.
+using Git: git
+using Git_jll: Git_jll
+using PkgSkeleton: PkgSkeleton
+using Preferences: Preferences
+
+# Configure `Git.jl`/`Git_jll.jl` to
+# use the local installation of git.
+using Preferences: Preferences
+function use_system_git!()
+  git_path = try
+    readchomp(`which git`)
+  catch
+    nothing
+  end
+  if !isnothing(git_path)
+    Preferences.set_preferences!("Git_jll", "git_path" => git_path)
+  end
+end
+
+# Get the default branch name.
+# This might be an alternative but it doesn't work for some reason:
+# ```julia
+# using LibGit2: LibGit2
+# LibGit2.get(AbstractString, LibGit2.GitConfig(), "init.defaultBranch")
+# ```
+function default_branch_name()
+  return try
+    readchomp(`$(git()) config --get init.defaultBranch`)
+  catch
+    "main"
+  end
+end
+
+function change_branch_name(path, branch_name)
+  original_dir = pwd()
+  cd(path)
+  original_branch_name = readchomp(`$(git()) branch --show-current`)
+  run(`$(git()) branch -m $original_branch_name $branch_name`)
+  cd(original_dir)
+  return nothing
+end
+
+function default_path()
+  # TODO: Use something like `joinpath(first(DEPOT_PATH), "dev", pkg_name)`
+  # to make it more general.
+  return joinpath(homedir(), ".julia", "dev")
+end
+
+function generate(pkg_name; path=default_path())
+  pkg_path = joinpath(path, pkg_name)
+  # TODO: Turn this into a keyword argument.
+  template_dir = joinpath(pkgdir(ITensorPkgSkeleton), "templates", "default")
+
+  branch_name = default_branch_name()
+  ## TODO: Allow customization of these, currently
+  ## they are hardcoded in the template.
+  user_replacements = Dict([
+    "GHUSER" => "ITensor",
+    "USERNAME" => "ITensor developers",
+    "USEREMAIL" => "support@itensor.org",
+  ])
+  PkgSkeleton.generate(
+    pkg_path;
+    templates=[template_dir],
+    user_replacements,
+  )
+
+  # Change the default branch.
+  change_branch_name(pkg_path, branch_name)
+  return nothing
+end
 
 end

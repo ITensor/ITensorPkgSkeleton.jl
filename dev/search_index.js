b0d676fc24f8b0375a096906cadbe7c8b6a68d39
Documenter.jl <documenter@juliadocs.github.io>
2024-11-21 17:47:09 +0000

    build based on 4fd5db8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorPkgSkeleton","category":"page"},{"location":"#ITensorPkgSkeleton","page":"Home","title":"ITensorPkgSkeleton","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorPkgSkeleton.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorPkgSkeleton]","category":"page"}]
+}

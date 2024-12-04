4372ca36d6f49b81bbe323a9d04dc1835ea29701
Lukas Devos <ldevos98@gmail.com>
2024-12-04 08:56:42 -0500

    apply generate

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,42 @@
-@eval module $(gensym())
-using Test: @testset
+using SafeTestSets
+using Suppressor: @suppress
 
-@testset "ITensorPkgSkeleton.jl" begin
-  filenames = filter(readdir(@__DIR__)) do f
-    startswith("test_")(f) && endswith(".jl")(f)
+# check for filtered groups
+# either via `--group=ALL` or through ENV["GROUP"]
+const pat = r"(?:--group=)(\w+)"
+arg_id = findfirst(contains(pat), ARGS)
+const GROUP = uppercase(
+  if isnothing(arg_id)
+    get(ENV, "GROUP", "ALL")
+  else
+    only(match(pat, ARGS[arg_id]).captures)
+  end,
+)
+
+@time begin
+  # tests in groups based on folder structure
+  for testgroup in filter(isdir, readdir(@__DIR__))
+    if GROUP == "ALL" || GROUP == uppercase(testgroup)
+      for file in readdir(joinpath(@__DIR__, testgroup); join=true)
+        @eval @safetestset "$file" begin
+          include(file)
+        end
+      end
+    end
   end
-  @testset "Test $filename" for filename in filenames
-    include(filename)
+
+  # single files in top folder
+  for file in filter(isfile, readdir(@__DIR__))
+    @eval @safetestset "$file" begin
+      include(file)
+    end
   end
-end
+
+  # test examples
+  examplepath = joinpath(@__DIR__, "..", "examples")
+  for file in filter(endswith(".jl"), readdir(examplepath; join=true))
+    @eval @safetestset "$file" begin
+      @suppress include(file)
+    end
+  end
 end
--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,9 +1,7 @@
-@eval module $(gensym())
 using ITensorPkgSkeleton: ITensorPkgSkeleton
 using Aqua: Aqua
 using Test: @testset
 
 @testset "Code quality (Aqua.jl)" begin
   Aqua.test_all(ITensorPkgSkeleton)
-end
 end

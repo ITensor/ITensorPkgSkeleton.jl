5869703de41404d9f5cd98a79c1cdf75ad76685f
Lukas Devos <ldevos98@gmail.com>
2024-12-09 13:48:25 -0500

    Add functionality for setup
    Revert "Add functionality for loading setup code"
    
    This reverts commit 5ed825c1735352666ec7f2426add171c5b52c1d4.
    
    setup file logic

--- templates/test/test/runtests.jl
+++ templates/test/test/runtests.jl
@@ -13,11 +13,12 @@ const GROUP = uppercase(
   end,
 )
 
-function istestfile(filename)
-  return isfile(filename) &&
-         endswith(filename, ".jl") &&
-         startswith(basename(filename), "test")
-end
+"match files of the form `test_*.jl`, but exclude `*setup*.jl`"
+istestfile(fn) =
+  endswith(fn, ".jl") && startswith(basename(fn), "test_") && !contains(fn, "setup")
+"match files of the form `*.jl`, but exclude `*_notest.jl` and `*setup*.jl`"
+isexamplefile(fn) =
+  endswith(fn, ".jl") && !endswith(fn, "_notest.jl") && !contains(fn, "setup")
 
 @time begin
   # tests in groups based on folder structure
@@ -33,7 +34,7 @@ end
 
   # single files in top folder
   for file in filter(istestfile, readdir(@__DIR__))
-    (file == basename(@__FILE__)) && continue
+    (file == basename(@__FILE__)) && continue # exclude this file to avoid infinite recursion
     @eval @safetestset $file begin
       include($file)
     end
@@ -41,16 +42,19 @@ end
 
   # test examples
   examplepath = joinpath(@__DIR__, "..", "examples")
-  for file in filter(endswith(".jl"), readdir(examplepath; join=true))
-    filename = basename(file)
-    @eval begin
-      @safetestset $filename begin
-        $(Expr(
-          :macrocall,
-          GlobalRef(Suppressor, Symbol("@suppress")),
-          LineNumberNode(@__LINE__, @__FILE__),
-          :(include($file)),
-        ))
+  for (root, _, files) in walkdir(examplepath)
+    contains(root, "setup") && continue
+    for file in filter(isexamplefile, files)
+      filename = joinpath(root, file)
+      @eval begin
+        @safetestset $file begin
+          $(Expr(
+            :macrocall,
+            GlobalRef(Suppressor, Symbol("@suppress")),
+            LineNumberNode(@__LINE__, @__FILE__),
+            :(include($filename)),
+          ))
+        end
       end
     end
   end

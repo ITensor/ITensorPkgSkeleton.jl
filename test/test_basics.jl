d8715c286cc010be313b1a088b75ce592ddc2a47
Matt Fishman <mtfishman@users.noreply.github.com>
2025-02-10 12:27:32 -0500

    Update downstream test template (#49)
    * Update downstream test template
    
    * Fix tests
    
    * Update formatting a bit
    
    * Fix tests

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -20,20 +20,23 @@ using Test: @test, @testset
     for dir in pkgdirs
       @test isdir(joinpath(path, "NewPkg", dir))
     end
-    @test !isfile(joinpath(path, "NewPkg", ".github", "workflows", "Downstream.yml"))
+    @test !isfile(joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml"))
   end
   @testset "generate with downstream tests" begin
     for templates in (ITensorPkgSkeleton.default_templates(), [])
-      for downstreampkgs in (["DownstreamPkg"], [(user="ITensor", repo="DownstreamPkg")])
+      for (downstreampkgs, ghuser) in
+          ((["DownstreamPkg"], "ITensor"), ([(ghuser="Org", repo="DownstreamPkg")], "Org"))
         path = mktempdir()
         ITensorPkgSkeleton.generate("NewPkg"; path, templates, downstreampkgs)
         @test isdir(joinpath(path, "NewPkg"))
         @test isdir(joinpath(path, "NewPkg", ".github", "workflows"))
-        @test isfile(joinpath(path, "NewPkg", ".github", "workflows", "Downstream.yml"))
+        @test isfile(
+          joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml")
+        )
         @test open(
-          joinpath(path, "NewPkg", ".github", "workflows", "Downstream.yml"), "r"
+          joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml"), "r"
         ) do io
-          return contains(read(io, String), "- {user: ITensor, repo: DownstreamPkg.jl}")
+          return contains(read(io, String), "- '$(ghuser)/DownstreamPkg.jl'")
         end
         for dir in setdiff(pkgdirs, [".github", ".github/workflows"])
           @test isdir(joinpath(path, "NewPkg", dir)) == !isempty(templates)

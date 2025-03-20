64061303f8b207af741ed95cc9df93780a4f86b8
mtfishman <mfishman@flatironinstitute.org>
2025-03-20 13:53:28 -0400

    Try fixing image in docs

--- docs/make_index.jl
+++ docs/make_index.jl
@@ -4,7 +4,7 @@ using ITensorPkgSkeleton: ITensorPkgSkeleton
 function ccq_logo(content)
   include_ccq_logo = """
     ```@raw html
-    <img src="assets/CCQ.png" width="10%" alt="Flatiron Center for Computational Quantum Physics logo.">
+    <img src="assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">
     ```
     """
   content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)

8dc0ec8bc39876abaf2db5c4a49c433ad5fac8ce
mtfishman <mfishman@flatironinstitute.org>
2025-03-20 11:39:26 -0400

    Try fixing including logo

--- docs/make_index.jl
+++ docs/make_index.jl
@@ -1,9 +1,20 @@
 using Literate: Literate
 using ITensorPkgSkeleton: ITensorPkgSkeleton
 
+function ccq_logo(content)
+  include_ccq_logo = """
+    ```@raw html
+    <img src="../assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">
+    ```
+    """
+  content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)
+  return content
+end
+
 Literate.markdown(
   joinpath(pkgdir(ITensorPkgSkeleton), "examples", "README.jl"),
   joinpath(pkgdir(ITensorPkgSkeleton), "docs", "src");
   flavor=Literate.DocumenterFlavor(),
   name="index",
+  preprocess=ccq_logo,
 )

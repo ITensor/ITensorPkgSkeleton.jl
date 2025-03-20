f13e80e36d83ab318435e8a55fa16be6a2c9a45a
mtfishman <mfishman@flatironinstitute.org>
2025-03-20 13:31:20 -0400

    Try fixing image in docs

--- docs/make_index.jl
+++ docs/make_index.jl
@@ -4,9 +4,12 @@ using ITensorPkgSkeleton: ITensorPkgSkeleton
 function ccq_logo(content)
   # Prepending an extra `..` to the path is required to circumvent:
   # https://github.com/JuliaDocs/Documenter.jl/issues/921
+
+  # <img src="../assets/CCQ.png" width="10%" alt="Flatiron Center for Computational Quantum Physics logo.">
+
   include_ccq_logo = """
     ```@raw html
-    <img src="../assets/CCQ.png" width="10%" alt="Flatiron Center for Computational Quantum Physics logo.">
+    <img src="../assets/CCQ.png">
     ```
     """
   content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)

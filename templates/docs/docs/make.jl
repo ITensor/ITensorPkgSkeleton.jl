9d7e331704c2e5b83985af66e26cc66104277d5b
mtfishman <mfishman@flatironinstitute.org>
2025-03-23 16:18:27 -0400

    Specify favicon in docs build

--- templates/docs/docs/make.jl
+++ templates/docs/docs/make.jl
@@ -14,7 +14,7 @@ makedocs(;
   format=Documenter.HTML(;
     canonical="https://ITensor.github.io/{PKGNAME}.jl",
     edit_link="main",
-    assets=String[],
+    assets=["assets/favicon.ico"],
   ),
   pages=["Home" => "index.md", "Reference" => "reference.md"],
 )

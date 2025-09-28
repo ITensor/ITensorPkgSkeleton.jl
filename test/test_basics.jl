3eb5df727a3540e9a2a2fbf9483f6ccd9626fd7d
Lukas Devos <ldevos98@gmail.com>
2025-09-28 08:27:11 -0400

    Runic formatter

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -2,45 +2,45 @@ using ITensorPkgSkeleton: ITensorPkgSkeleton
 using Test: @test, @testset
 
 @testset "ITensorPkgSkeleton" begin
-  pkgdirs = [
-    ".github",
-    ".github/ISSUE_TEMPLATE",
-    ".github/workflows",
-    "benchmark",
-    "docs",
-    "examples",
-    "src",
-    "test",
-  ]
-  @testset "generate" begin
-    path = mktempdir()
-    ITensorPkgSkeleton.generate("NewPkg"; path)
-    @test isdir(joinpath(path, "NewPkg"))
-    @test isfile(joinpath(path, "NewPkg", "Project.toml"))
-    for dir in pkgdirs
-      @test isdir(joinpath(path, "NewPkg", dir))
-    end
-    @test !isfile(joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml"))
-  end
-  @testset "generate with downstream tests" begin
-    for templates in (ITensorPkgSkeleton.default_templates(), [])
-      for downstreampkgs in (["DownstreamPkg"],)
+    pkgdirs = [
+        ".github",
+        ".github/ISSUE_TEMPLATE",
+        ".github/workflows",
+        "benchmark",
+        "docs",
+        "examples",
+        "src",
+        "test",
+    ]
+    @testset "generate" begin
         path = mktempdir()
-        ITensorPkgSkeleton.generate("NewPkg"; path, templates, downstreampkgs)
+        ITensorPkgSkeleton.generate("NewPkg"; path)
         @test isdir(joinpath(path, "NewPkg"))
-        @test isdir(joinpath(path, "NewPkg", ".github", "workflows"))
-        @test isfile(
-          joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml")
-        )
-        @test open(
-          joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml"), "r"
-        ) do io
-          return contains(read(io, String), "- 'DownstreamPkg'")
+        @test isfile(joinpath(path, "NewPkg", "Project.toml"))
+        for dir in pkgdirs
+            @test isdir(joinpath(path, "NewPkg", dir))
         end
-        for dir in setdiff(pkgdirs, [".github", ".github/workflows"])
-          @test isdir(joinpath(path, "NewPkg", dir)) == !isempty(templates)
-        end
-      end
+        @test !isfile(joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml"))
     end
-  end
+    @testset "generate with downstream tests" begin
+        for templates in (ITensorPkgSkeleton.default_templates(), [])
+            for downstreampkgs in (["DownstreamPkg"],)
+                path = mktempdir()
+                ITensorPkgSkeleton.generate("NewPkg"; path, templates, downstreampkgs)
+                @test isdir(joinpath(path, "NewPkg"))
+                @test isdir(joinpath(path, "NewPkg", ".github", "workflows"))
+                @test isfile(
+                    joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml")
+                )
+                @test open(
+                    joinpath(path, "NewPkg", ".github", "workflows", "IntegrationTest.yml"), "r"
+                ) do io
+                    return contains(read(io, String), "- 'DownstreamPkg'")
+                end
+                for dir in setdiff(pkgdirs, [".github", ".github/workflows"])
+                    @test isdir(joinpath(path, "NewPkg", dir)) == !isempty(templates)
+                end
+            end
+        end
+    end
 end

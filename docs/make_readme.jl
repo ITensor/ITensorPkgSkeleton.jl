7386854d45c9a91e9eebd0e1ab55b138ce97891f
mtfishman <mfishman@flatironinstitute.org>
2026-02-13 13:39:51 -0500

    Format

--- docs/make_readme.jl
+++ docs/make_readme.jl
@@ -1,5 +1,5 @@
-using Literate: Literate
 using ITensorPkgSkeleton: ITensorPkgSkeleton
+using Literate: Literate
 
 function ccq_logo(content)
     include_ccq_logo = """
@@ -17,5 +17,5 @@ Literate.markdown(
     joinpath(pkgdir(ITensorPkgSkeleton));
     flavor = Literate.CommonMarkFlavor(),
     name = "README",
-    postprocess = ccq_logo,
+    postprocess = ccq_logo
 )

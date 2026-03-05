5ca0dce904a1be4ee8f9fa1ce93318cda3fff69a
mtfishman <mfishman@flatironinstitute.org>
2026-03-04 21:44:23 -0500

    Fix mistake and simplify

--- src/ITensorPkgSkeleton.jl
+++ src/ITensorPkgSkeleton.jl
@@ -182,16 +182,8 @@ function runtests(; testdir::AbstractString, args = ARGS, env = ENV)
                 contains(chopprefix(root, testdir), "setup") && continue
                 for file in filter(_isexamplefile, files)
                     filename = joinpath(root, file)
-                    buf = IOBuffer()
-                    try
-                        redirect_stdout(buf) do
-                            redirect_stderr(buf) do
-                                return _run_isolated_testfile(filename; label = file)
-                            end
-                        end
-                    catch
-                        print(stderr, String(take!(buf)))
-                        rethrow()
+                    redirect_stdout(devnull) do
+                        return _run_isolated_testfile(filename; label = file)
                     end
                 end
             end
